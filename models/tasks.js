b33628f5974e9c37b4c09dec5fd6d19a6c9d59e5
marcelo gobelli <marcelo@localhost.localdomain>
2017-02-02 20:08:22 -0500

    using lab_inventory table

--- models/tasks.js
+++ models/tasks.js
@@ -1,16 +1,101 @@
 "use strict"
 
 module.exports = function(sequelize, DataTypes) {
-    var my_db = sequelize.define("test_marcelo", {
-        title: {
-            type: DataTypes.STRING,
+    var my_db = sequelize.define("lab_inventory", {
+        
+    	business_unit: {
+		type: DataTypes.STRING,
 	},
-        completed: {
+	lab_name: {
+		type: DataTypes.STRING,
+	},
+	location: {
+		type: DataTypes.STRING,
+	},
+	validated: {
             type: DataTypes.BOOLEAN,
             
-	}
-    }, {
-       tableName: 'test_marcelo'
+	},
+	computer_name: {
+            type: DataTypes.STRING,
+	allowNull: true
+            
+	},
+	bmrn_asset: {
+            type: DataTypes.STRING,
+            
+	},	
+	pc_make_model: {
+            type: DataTypes.STRING,
+            
+	},
+	os: {
+            type: DataTypes.STRING,
+            
+	},
+	notes: {
+            type: DataTypes.STRING,
+            
+	},
+	system_owner: {
+            type: DataTypes.STRING,
+            
+	},
+	vendor: {
+            type: DataTypes.STRING,
+            
+	},
+	model_instrument_name: {
+            type: DataTypes.STRING,
+            
+	},
+	instrument_friendly_name: {
+            type: DataTypes.STRING,
+            
+	},
+	instrument_validation_id: {
+            type: DataTypes.STRING,
+            
+	},
+	instrument_software: {
+            type: DataTypes.STRING,
+            
+	},
+	backup_data_format: {
+            type: DataTypes.STRING,
+            
+	},
+	nic_address: {
+            type: DataTypes.STRING,
+            
+	},
+	ethernet_jack: {
+            type: DataTypes.STRING,
+            
+	},
+	ip_address_vlan: {
+            type: DataTypes.STRING,
+            
+	},
+	image: {
+            type: DataTypes.STRING,
+            
+	},
+	backup: {
+            type: DataTypes.STRING,
+            
+	},
+	decommisioned: {
+            type: DataTypes.STRING,
+            
+	},
+	last_verified: {
+            type: DataTypes.STRING,
+            
+	},										
+			
+}, {
+       tableName: 'lab_inventory'
     });
 
     return my_db;
