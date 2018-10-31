cce32a39ee2ae6e929debbce8759c85bb2adf67a
Marcelo Gobelli <marcelo.gobelli@gmail.com>
2018-10-31 13:10:40 -0700

    working after a while on this

--- routes/login.js
+++ routes/login.js
@@ -11,6 +11,11 @@ exports.destroySession = function(req, res, next) {
 	console.log("logout success!");
 	req.logOut();
 	req.session.destroy();
+	//res.cookie("session.id", "", { expires: new Date() });
 	res.redirect("/");
 
 }
+
+
+//res.setCookie in destroySession before the redirect....delete a cookie and express how to delete 
+//a cookie....
